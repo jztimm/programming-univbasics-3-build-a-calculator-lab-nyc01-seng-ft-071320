dbe05e117943885079910bf7f71c20bc5181a7f2
Jzavier Timm <jztimm@gmail.com>
2020-06-29 02:09:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,18 @@
 def addition(num1, num2)
-  puts num1 + num2
+  puts num2 + num1
 end
 
 def subtraction(num1, num2)
-  puts num1 - num2
+  puts num2 - num1
 end
 
 def division(num1, num2)
-  puts num1 / num2
+  puts num2 / num1
 end
 
 def multiplication(num1, num2)
-  puts num1 * num2
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
